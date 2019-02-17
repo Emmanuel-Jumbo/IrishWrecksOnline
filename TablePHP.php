5c0cc25ab8a6324d6f406d87e1dfee7a5e0ea1d4
MBoush <47496027+MBoush@users.noreply.github.com>
2019-02-17 21:17:23 +0400

    Add files via upload

---
+++ TablePHP.php
@@ -0,0 +1,293 @@
+<html> <!--47,25,95,1.0--><!--115,83,186,1.0--><!--colors in website converted to rgba-->
+	<head>
+		<style>
+			table
+			{
+				width:100%;
+				text-align:center;
+				border:5px;
+				border-radius: 20px, 5px, 20px, 5px;
+			}
+			tr:nth-child(even)
+			{
+				background-color:rgba(115,83,186,0.5);
+			}
+			tr:nth-child(odd)
+			{
+				background-color:rgba(115,83,186,1.0);
+			}
+			th
+			{
+				border:1px rgba(0,0,0,0.2);
+				padding:30px;
+				background-color:rgba(47,25,95,0.7);
+				border-radius:20px 5px 20px 5px;
+			}
+			td
+			{
+				border:1px rgba(255,255,255,1);
+				padding:15px;
+				border-radius:5px 20px 5px 20px;
+			}
+			tr:hover
+			{
+				background-color:rgba(30,150,255,0.3);
+			}
+		</style>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<th>
+					Name
+				</th>
+				<th>
+					Vessel Type
+				</th>
+				<th>
+					Date Of Loss
+				</th>
+				<th>
+					Cause Of Loss
+				</th>
+				<th>
+					Location
+				</th>
+			</tr>
+			<tr>
+				<!--a href="http://www.irishwrecksonline.net/details/Shamrock689.htm"></a>-->
+				<td>
+					S.S. Shamrock II
+				</td>
+				<td>
+					Cargo Steamer
+				</td>
+				<td>
+					5th May 1918
+				</td>
+				<td>
+					Ran Ashore
+				</td>
+				<td>
+					Co. Dublin
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/Lusitania608.htm"></a>-->
+				<td>
+					S.S. Lusitania
+				</td>
+				<td>
+					Liner
+				</td>
+				<td>
+					7th May 1915
+				</td>
+				<td>
+					Torpedoed
+				</td>
+				<td>
+					Co. Cork
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/EmpireHeritage358.htm"></a>-->
+				<td>
+					S.S. Empire Heritage
+				</td>
+				<td>
+					Steam Tanker
+				</td>
+				<td>
+					8th September 1944
+				</td>
+				<td>
+					Torpedoed
+				</td>
+				<td>
+					Co. Donegal
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/Celtic134.htm"></a>-->
+				<td>
+					S.S. Cletic
+				</td>
+				<td>
+					Liner
+				</td>
+				<td>
+					10th December 1928
+				</td>
+				<td>
+					Struck Rocks
+				</td>
+				<td>
+					Co. Cork
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/StateOfLouisiana700.htm"></a>-->
+				<td>
+					S.S. State of Louisiana
+				</td>
+				<td>
+					3 Masted Steamer
+				</td>
+				<td>
+					24th December 1878
+				</td>
+				<td>
+					Struck Rocks
+				</td>
+				<td>
+					Co. Antrim
+				</td>
+			</tr>
+			<tr>
+				<!--<a href=http://www.irishwrecksonline.net/details/Thrush775.htm"></a>-->
+				<td>
+					S.S. Thrush
+				</td>
+				<td>
+					Salvage Steamer
+				</td>
+				<td>
+					11th April 1917
+				</td>
+				<td>
+					Foundered
+				</td>
+				<td>
+					Co. Antrim
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/Towy800.htm"></a>-->
+				<td>
+					S.S. Towy
+				</td>
+				<td>
+					Steam Coater
+				</td>
+				<td>
+					20th June 1930
+				</td>
+				<td>
+					Holed
+				</td>
+				<td>
+					Co. Antrim
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/Carnavonshire124.htm"></a>-->
+				<td>
+					S.V. Carnavonshire
+				</td>
+				<td>
+					3 Masted Sail Ship
+				</td>
+				<td>
+					11th April 1896
+				</td>
+				<td>
+					Struck Rocks
+				</td>
+				<td>
+					Co. Cork
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/Clemintine200.htm"></a>-->
+				<td>
+					S.Y. Clemintine
+				</td>
+				<td>
+					Steam Yacht
+				</td>
+				<td>
+					1918 Approx.
+				</td>
+				<td>
+					Foundered
+				</td>
+				<td>
+					Co. Antrim
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/Leinster555.htm"></a>-->
+				<td>
+					R.M.S. Leinster
+				</td>
+				<td>
+					Steamer
+				</td>
+				<td>
+					10th October 1918
+				</td>
+				<td>
+					Torpedoed
+				</td>
+				<td>
+					Co. Dublin
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/Justicia544ayy.htm"></a>-->
+				<td>
+					R.M.S. Justicia
+				</td>
+				<td>
+					White Star Liner
+				</td>
+				<td>
+					19th July 1918
+				</td>
+				<td>
+					Torpedoed
+				</td>
+				<td>
+					Co. Donegal
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/KowloonBridge548.htm"></a>-->
+				<td>
+					M.V. Kowloon Bridge
+				</td>
+				<td>
+					Super Bulk Carrier
+				</td>
+				<td>
+					22nd November 1986
+				</td>
+				<td>
+					Ran Aground In Gale
+				</td>
+				<td>
+					Co. Cork
+				</td>
+			</tr>
+			<tr>
+				<!--<a href="http://www.irishwrecksonline.net/details/BardiniReefer115a.htm"></a>-->
+				<td>
+					M.V. Bardini Reefer
+				</td>
+				<td>
+					Motor Coaster
+				</td>
+				<td>
+					15th December 1982
+				</td>
+				<td>
+					Fire And Explosion
+				</td>
+				<td>
+					Co. Cork
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
